0c6b0889d1531b89f1819507fbee31b35f267c06
forax <forax@univ-mlv.fr>
2023-12-05 00:48:06 +0100

    Move the codes of Day 1 around to be layout like the other days

--- src/main/java/AdventOfCode01.java
+++ src/main/java/AdventOfCode01.java
@@ -1,15 +1,18 @@
 public class AdventOfCode01 {
+  static int sumOfDigits(String input) {
+    return input.lines()
+        .map(line -> line.chars().filter(c -> c >= '0' && c <= '9').mapToObj(c -> c - '0').toList())
+        .mapToInt(list -> 10 * list.getFirst() + list.getLast())
+        .sum();
+  }
+
   public static void main(String[] args) {
-    var text = """
+    var input = """
         1abc2
         pqr3stu8vwx
         a1b2c3d4e5f
         treb7uchet
         """;
-    var result = text.lines()
-        .map(line -> line.chars().filter(c -> c >= '0' && c <= '9').mapToObj(c -> c - '0').toList())
-        .mapToInt(list -> 10 * list.getFirst() + list.getLast())
-        .sum();
-    System.out.println(result);
+    System.out.println(sumOfDigits(input));
   }
 }
