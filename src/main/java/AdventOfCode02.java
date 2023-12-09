d289d348d9dbadef586e2e6003dc7857dae110e6
forax <forax@univ-mlv.fr>
2023-12-09 09:02:48 +0100

    Far simpler solution for Day 2

--- src/main/java/AdventOfCode02.java
+++ src/main/java/AdventOfCode02.java
@@ -1,3 +1,4 @@
+import java.util.Map;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
@@ -6,33 +7,16 @@ import static java.util.regex.Pattern.compile;
 import static java.util.stream.IntStream.range;
 
 public class AdventOfCode02 {
-  private static final Pattern PATTERN = compile("(?:Game ([0-9]+))|(?:([0-9]+) red)|(?:([0-9]+) green)|(?:([0-9]+) blue)|(;)");
+  private static final Pattern PATTERN = Pattern.compile("Game ([0-9]+)|([0-9]+) (red|green|blue)");
 
-  static int sumOfGameId(String text, int expectedRed, int expectedGreen, int expectedBlue) {
-    return text.lines()
-        .mapToInt(l -> {
-          var matcher = PATTERN.matcher(l);
+  static int sumOfGameId(String input, int expectedRed, int expectedGreen, int expectedBlue) {
+    var expected = Map.of("red", expectedRed, "green", expectedGreen, "blue", expectedBlue);
+    return input.lines()
+        .mapToInt(line -> {
+          var matcher = PATTERN.matcher(line);
           matcher.find();
-          var id = parseInt(matcher.group(1));
-          record Draw(int red, int green, int blue) {}
-          return Stream.of(l)
-              .<Draw>mapMulti((line, consumer) -> {
-                int red = 0, green = 0, blue = 0;
-                while (matcher.find()) {
-                  var kind = range(0, matcher.groupCount()).filter(i -> matcher.start(i + 1) != -1).findFirst().orElse(-1);
-                  switch (kind) {
-                    case 1 -> red = parseInt(matcher.group(2));
-                    case 2 -> green = parseInt(matcher.group(3));
-                    case 3 -> blue = parseInt(matcher.group(4));
-                    case 4 -> {
-                      consumer.accept(new Draw(red, green, blue));
-                      red = green = blue = 0;
-                    }
-                  }
-                }
-                consumer.accept(new Draw(red, green, blue));
-              })
-              .allMatch(draw -> draw.red <= expectedRed && draw.green <= expectedGreen && draw.blue <= expectedBlue) ? id : 0;
+          var gameId = parseInt(matcher.group(1));
+          return matcher.results().allMatch(result -> parseInt(result.group(2)) <= expected.get(result.group(3))) ? gameId : 0;
         })
         .sum();
   }
