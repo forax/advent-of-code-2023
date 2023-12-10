1f14b50969fce60fa7f10fd52ce1c7269c3f97e4
forax <forax@univ-mlv.fr>
2023-12-10 21:57:46 +0100

    typo, a missing whitespace

--- src/main/java/AdventOfCode08.java
+++ src/main/java/AdventOfCode08.java
@@ -14,7 +14,7 @@ public class AdventOfCode08 {
     var instrs = scanner.nextLine().chars().map(c -> c == 'L' ? 0 : 1).toArray();
     record Node(String name, String... next) {}
     var map = scanner.findAll(PATTERN)
-        .map(result -> new Node(result.group(1),result.group(2), result.group(3)))
+        .map(result -> new Node(result.group(1), result.group(2), result.group(3)))
         .collect(toMap(Node::name, n -> n));
     var cycle = Stream.iterate(0, i -> (i + 1) % instrs.length).iterator();
     return (int) Stream.iterate("AAA", not("ZZZ"::equals), n -> map.get(n).next[instrs[cycle.next()]]).count();
